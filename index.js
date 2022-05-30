0e1f1ad31538c87da13b3dc0322214097dcba1d9
camillyPessotti <camilly_pessotti@estudante.sc.senai.br>
2022-05-30 18:19:36 -0300

    commit

--- index.js
+++ index.js
@@ -4,50 +4,50 @@ const port = 3000;
 
 app.use(express.json());
 
-const listaPessoas = [
-    { id: 1, nome: "Camilly", idade: 17 },
-    { id: 2, nome: "Bruna", idade: 17 },
-    { id: 3, nome: "João", idade: 17 }
-];
+// const listaPessoas = [
+//     { id: 1, nome: "Camilly", idade: 17 },
+//     { id: 2, nome: "Bruna", idade: 17 },
+//     { id: 3, nome: "João", idade: 17 }
+// ];
 
-app.get('/', (req, res) => {
-    res.send("Hello world!");
-});
+// app.get('/', (req, res) => {
+//     res.send("Hello world!");
+// });
 
-app.get('/api/pessoas', (req, res) => {
-    console.log(req);
+// app.get('/api/pessoas', (req, res) => {
+//     console.log(req);
 
-    res.send(listaPessoas);
-});
+//     res.send(listaPessoas);
+// });
 
-app.post('/api/pessoas', (req, res) => {
-    const pessoa = req.body;
-    pessoa.id = listaPessoas.length + 1;
-    listaPessoas.push(pessoa);
-    res.json(pessoa);
-});
+// app.post('/api/pessoas', (req, res) => {
+//     const pessoa = req.body;
+//     pessoa.id = listaPessoas.length + 1;
+//     listaPessoas.push(pessoa);
+//     res.json(pessoa);
+// });
 
-app.put('/api/pessoas/:id', (req, res) => {
-    const id = req.params.id;
-    const pessoa = req.body;
-    const index = listaPessoas.findIndex(p => p.id == id);
-    pessoa.id = id;
-    listaPessoas[index] = pessoa;
-    res.json(pessoa);
-});
+// app.put('/api/pessoas/:id', (req, res) => {
+//     const id = req.params.id;
+//     const pessoa = req.body;
+//     const index = listaPessoas.findIndex(p => p.id == id);
+//     pessoa.id = id;
+//     listaPessoas[index] = pessoa;
+//     res.json(pessoa);
+// });
 
-app.get('/api/pessoas/:id', (req, res) => {
-    const id = req.params.id;
-    const pessoa = listaPessoas.find(p => p.id == id);
-    res.send(pessoa);
-});
+// app.get('/api/pessoas/:id', (req, res) => {
+//     const id = req.params.id;
+//     const pessoa = listaPessoas.find(p => p.id == id);
+//     res.send(pessoa);
+// });
 
-app.delete('/api/pessoas/:id', (req, res) => {
-    const id = req.params.id;
-    const index = listaPessoas.findIndex(p => p.id == id);
-    listaPessoas.splice(index, 1);
-    res.json(listaPessoas);
-});
+// app.delete('/api/pessoas/:id', (req, res) => {
+//     const id = req.params.id;
+//     const index = listaPessoas.findIndex(p => p.id == id);
+//     listaPessoas.splice(index, 1);
+//     res.json(listaPessoas);
+// });
 
 
 const listaUsuarios = [
